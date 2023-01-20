39b94f07a9e5868fd8bd7ad9d035a316b8ffa645
am001541 <mohamed.amlah@etu.unice.fr>
2023-01-20 18:57:23 +0100

    Version a jour Fonctionnelle

--- www/JS/slider.js
+++
@@ -1,9 +0,0 @@
-
-const slide = ["/images/desktopA.jpg", "/images/gaming.jpg"];
-let numero = 0;
-function changeSlide(){
-    while(slide.length < 0){
-      numero++;
-    }
-    document.getElementById("container").src = slide[numero];
-}
--- www/PHP/generalPartPage/footerGeneral.php
+++
@@ -1,6 +0,0 @@
-<div class="prefooter"></div>
-<footer>
-    <p class="footer-p">
-        Copyright © 2022 . All rights reserved. - <a href="legalInfo.php">Legal information</a>
-    </p>
-</footer>
